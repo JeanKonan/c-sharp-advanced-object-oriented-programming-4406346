7a8da9c9c8600034033dc9e3f107c5d3d8f28f5d
Jean Y Konan <104398724+JeanKonan@users.noreply.github.com>
2025-07-09 01:03:26 +0000

    ch1 completed

--- Start/Ch1/AnonTypes/Program.cs
+++ Start/Ch1/AnonTypes/Program.cs
@@ -2,12 +2,27 @@
 // Creating and using Anonymous types
 
 // TODO: Anonymous types can be declared using "new" and { }, similar to JS
+using System.Net.Sockets;
 
-
+var myobj = new
+{
+    Name = "Jean Konan",
+    Age = 27,
+    AddressFamily = new
+    {
+        Street = "123 Main St",
+        City = "Anywhere"
+    }
+};
+// Console.WriteLine($"{myobj.Name}, {myobj.AddressFamily.Street}");
+// Console.WriteLine($"{myobj}");
 // Anonymous types are read-only and cannot be modified
 // myobj.Name = "Jane Doe"; # will cause an error
 
 // TODO: To change a value, use non-destructive mutation and "with" clause
-
+// var myobj2 = myobj with { Name = "Jane Doe" };
+// Console.WriteLine($"{myobj2.Name}, {myobj2.AddressFamily.Street}");
 
 // TODO: You can check to see if an anonymous object contains a property
+Console.WriteLine($"{myobj.GetType().GetProperty("Name") != null}");
+Console.WriteLine($"{myobj.GetType().GetProperty("Job") != null}");
