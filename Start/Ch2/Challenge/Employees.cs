e1ebb9e74ec9e38b9748beac0e386bab9fea63cd
Jean Y Konan <104398724+JeanKonan@users.noreply.github.com>
2025-07-10 03:23:21 +0000

    ch2 completed

--- Start/Ch2/Challenge/Employees.cs
+++ Start/Ch2/Challenge/Employees.cs
@@ -5,8 +5,42 @@ public class Employee {
     public Employee() {}
 
     // YOUR CODE GOES HERE
+    private int _id;
 
+    public required int ID
+    {
+        get => _id;
+        init => _id = value;
+    }
+    public required string Department { get; set; }
+    public required string FullName { get; set; }
+
+    public virtual void AdjustPay(decimal percentage) {}
     public override string ToString() => $"{ID}:{FullName}, {Department} ";
 }
 
 // DEFINE OTHER CLASSES HERE
+
+class HourlyEmployee : Employee
+{
+    public decimal PayRate { get; set; }
+
+    public override void AdjustPay(decimal percentage)
+    {
+        PayRate += PayRate * percentage;
+    }
+
+    public HourlyEmployee() { }
+}
+
+class SalariedEmployee : Employee
+{
+    public decimal Salary { get; set; }
+
+    public override void AdjustPay(decimal percentage)
+    {
+        Salary += Salary * percentage;
+    }
+
+    public SalariedEmployee() { }
+}
